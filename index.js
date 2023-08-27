e82546dff77b529673886b0b0cd5f24a6dd14b47
Lokesh Patidar <Lokeshpati84@gmail.com>
2023-08-28 02:41:32 +0530

    Complete all the remaining sections

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+fetch("https://fakestoreapi.com/products")
+  .then((data) => {
+    return data.json();
+  })
+  .then((completeData) => {
+    let data1 = "";
+    completeData.map((values) => {
+      data1 += `<div class="card">
+          <img class="images" src="${values.image}" alt="image">
+          <h1 class="title">${values.title}</h1>
+          <p class="description">${values.description}</p>
+          <p class="price">${"$" + values.price}</p>
+        </div>`;
+    });
+    document.getElementById("cards").innerHTML = data1;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
